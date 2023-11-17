1a25d87488b090ef59a41bd4fac8c511780e22b4
Jose Velasco <josevelasco1022@gmail.com>
2023-11-16 19:00:28 -0800

    NextJS Image Component Logos1/2

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Logos() {
   return (
     <div className="bg-white py-24 sm:py-12">
@@ -6,35 +8,35 @@ export default function Logos() {
           Tech Stack
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="mongodb.svg"
             alt="Transistor"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="express.svg"
             alt="Reform"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="react.svg"
             alt="Tuple"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
             src="nodejs.svg"
             alt="SavvyCal"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
             src="redux.svg"
             alt="Statamic"
--- src/components/Logos2.tsx
+++ src/components/Logos2.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Logos2() {
   return (
     <div className="bg-white py-24 sm:py-12">
@@ -6,35 +8,35 @@ export default function Logos2() {
         Adding to My Toolkit
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="next.svg"
             alt="Transistor"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="typescript.svg"
             alt="Reform"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="prisma.svg"
             alt="Tuple"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
             src="tailwind.svg"
             alt="SavvyCal"
             width={158}
             height={48}
           />
-          <img
+          <Image
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
             src="mysql.svg"
             alt="Statamic"
